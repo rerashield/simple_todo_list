64ca4361b6928926a95aceb9590b3654bd1997eb
Clement Chung <cthchung@ogcio.gov.hk>
2020-09-21 17:59:43 +0800

    Updated: Created the form

--- src/components/todolist.tsx
+++ src/components/todolist.tsx
@@ -3,9 +3,102 @@ import { TodoItem } from './todoItem';
 // import * as TodoItem from 'todoItem';
 
 
-class Todolist extends React.Component{
+// Class definition to declare the state values
+class Todolist extends React.Component<{}, { title: string, content: string }> {
     todoItems: Array<TodoItem> = [];
 
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            title: '',
+            content: ''
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+
+        this.addElements();
+    }
+
+
+    createInputForm() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Title:
+                        <input type="text" id="title" value={this.state.title} onChange={this.handleChange} />
+                    </label>
+                    <br></br>
+                    <label>
+                        Content:
+                        <input type="text" id="content" value={this.state.content} onChange={this.handleChange} />
+                    </label>
+                    <input type="submit" value="Submit" />
+                </form>
+            </div>
+        )
+    }
+
+    // Changing state to re-rendering the page
+    // Using the following method to specify the property needed to change
+    handleChange(e: React.FormEvent<HTMLInputElement>) {
+        var key: string = e.currentTarget.id;
+        var value: string = e.currentTarget.value;
+        var obj: any = {};
+        obj[key] = value;
+        //console.log(key);
+        this.setState(obj);
+    }
+
+    // After handled the input, reset the state value to re-rendering the page
+    handleSubmit(e: React.FormEvent) {
+        this.addElement(this.state.title, this.state.content);
+        e.preventDefault()
+        //console.log(this.todoItems);
+        this.setState({ title: '', content: '' });
+    }
+
+
+    addElements() {
+        this.todoItems.push(new TodoItem('A', 'B', new Date()));
+        this.todoItems.push(new TodoItem('D', 'B', new Date()));
+        this.todoItems.push(new TodoItem('A', 'C', new Date()));
+        this.todoItems.push(new TodoItem('F', 'F', new Date()));
+    }
+
+    addElement(title: String, content: String) {
+        this.todoItems.push(new TodoItem(title, content, new Date()));
+    }
+
+    render() {
+        //this.addElements();
+        // For each needed to get the index as the unique key
+        // table needed a tbody to store the tr
+        return (
+            <div>
+                {this.createInputForm()}
+                <table>
+                    <tbody>
+                        <tr>
+                            <th>Finished?</th>
+                            <th>Title</th>
+                            <th>Content</th>
+                            <th>Create Time</th>
+                        </tr>
+
+                        {this.todoItems.map((t, index) =>
+                            <tr key={index}>
+                                <td>{t.isFinished()}</td>
+                                <td>{t.getTitle()}</td>
+                                <td>{t.getContent()}</td>
+                            </tr>
+                        )}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
 }
 
 export { Todolist }
--- src/containers/all_item.tsx
+++ src/containers/all_item.tsx
@@ -1,39 +1,21 @@
 import * as React from 'react';
 import * as LandingMenu from '../components/landing_menu'
 import { TodoItem } from '../components/todoItem'
+import * as todoList from '../components/todolist'
 
 
 
 class AllItem extends React.Component {
     alpha: TodoItem = new TodoItem('A', 'B', new Date());
 
-    createList() {
-        return (
-            <div>
-                <table>
-                    <tr>
-                        <th>Finished?</th>
-                        <th>Title</th>
-                        <th>Content</th>
-                        <th>Create Time</th>
-                    </tr>
 
-                    <tr>
-                        <td>{this.alpha.isFinished()}</td>
-                        <td>{this.alpha.getTitle()}</td>
-                        <td>{this.alpha.getContent()}</td>
-                    </tr>
-                </table>
-            </div>
-        );
-    };
 
     render() {
         return (
             <div>
                 <h1 className="text-primary">Hello React as TSX all item</h1>
                 <LandingMenu.LandingPageMenu></LandingMenu.LandingPageMenu>
-                {this.createList()}
+                <todoList.Todolist></todoList.Todolist>
             </div>
         );
     };
